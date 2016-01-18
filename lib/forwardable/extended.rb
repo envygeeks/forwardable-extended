a74f94ea0174c8f0e2c4f2ff4e3eb16b4c091529
Jordon Bedwell <jordon@envygeeks.io>
2016-01-18 05:04:35 -0600

    Add some comment headers.

--- lib/forwardable/extended.rb
+++ lib/forwardable/extended.rb
@@ -1,6 +1,8 @@
+# ----------------------------------------------------------------------------
 # Frozen-string-literal: true
 # Copyright: 2015-2016 Jordon Bedwell - MIT License
 # Encoding: utf-8
+# ----------------------------------------------------------------------------
 
 require "forwardable/extended/version"
 require "forwardable"
@@ -9,9 +11,11 @@ module Forwardable
   module Extended
     DEF_DELEGATOR = Object::Forwardable.instance_method(:def_delegator)
 
+    # ------------------------------------------------------------------------
     # Delegates a method to a `hash[key]`.
     # @param [Symbol] key used if method is an alias; disignates the hash key.
     # @param [Hash] hash the hash object you wish to delegate to.
+    # ------------------------------------------------------------------------
 
     def def_hash_delegator(hash, method, key: method, **kwd)
       prefix, suffix, wrap = __prepare(**kwd)
@@ -23,10 +27,12 @@ module Forwardable
       STR
     end
 
+    # ------------------------------------------------------------------------
     # Delegates a method to an instance variable.
     # @note if you are not using an alias or booleans use `attr_reader`.
     # @param [String, Symbol] ivar the instance variable.
     # @param [String, Symbol] alias_ the alias.
+    # ------------------------------------------------------------------------
 
     def def_ivar_delegator(ivar, alias_ = ivar, **kwd)
       prefix, suffix, wrap = __prepare(**kwd)
@@ -38,11 +44,13 @@ module Forwardable
       STR
     end
 
+    # ------------------------------------------------------------------------
     # A more beefed up version of Ruby's `def_delegator` that
     # offers a tiny bit more than the default version in `Forwardable`
     # @param [Object<>] accessor the object to ship your method to.
     # @param [String, Symbol] method the method being messaged.
     # @param [Array<>] args the arguments to place in front.
+    # ------------------------------------------------------------------------
 
     def def_modern_delegator(accessor, method, alias_ = method, args: [], **kwd)
       args = [args].flatten.compact.map(&:to_s).unshift("").join(", ")
@@ -55,9 +63,11 @@ module Forwardable
       STR
     end
 
+    # ------------------------------------------------------------------------
     # Wraps around traditional def_delegator to offer forwarding to modern,
     # ivar and hash delegators.  With a bit of data checking between.
     # @see `Object::Forwardable#def_delegator`
+    # ------------------------------------------------------------------------
 
     def def_delegator(accessor, method, alias_ = method, **kwd)
       kwd, alias_ = alias_, method if alias_.is_a?(Hash) && !kwd.any?
@@ -79,8 +89,10 @@ module Forwardable
       end
     end
 
+    # ------------------------------------------------------------------------
     # Wraps around traditional `def_delegators` to detect hash arguments.
     # @see `Object::Forwardable#def_delegators`
+    # ------------------------------------------------------------------------
 
     def def_delegators(accessor, *methods)
       kwd = methods.shift if methods.first.is_a?(Hash)
@@ -92,9 +104,11 @@ module Forwardable
       end
     end
 
+    # ------------------------------------------------------------------------
     # Prepares the suffix, prefix and wrap method if available.
     # @param [true, false, :reverse] bool whether or not this is a boolean.
     # @param [true, Symbol, String] wrap wrap result into the wrap.
+    # ------------------------------------------------------------------------
 
     private
     def __prepare(wrap: nil, bool: false)
